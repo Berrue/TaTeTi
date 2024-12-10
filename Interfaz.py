ec3c32fac7dea6acfba1f8542ef959c8572683f2
Javier Cabral y Vedia <65092745+JavierCyV@users.noreply.github.com>
2024-12-10 00:02:31 -0300

    Agragado de click, falta pulir

--- Interfaz.py
+++ Interfaz.py
@@ -1,108 +1,138 @@
 import pygame
 import sys
 from pygame.locals import *
+import tkinter as tk
+
 
 pygame.init()
-
 pygame.display.set_caption('Tres en linea')
-
 pantalla = pygame.display.set_mode((500, 400))
-
-
 blanco = (255, 255, 255)
 negro = (0,0,0)
 rojo = (255,0,0)
-
 pantalla.fill(rojo)
 
-circulo = pygame.image.load('circulo.png')
-equis = pygame.image.load('cruz.png')
 
-clock = pygame.time.Clock()
-
-
-pygame.draw.rect(pantalla, blanco, (160, 42, 10, 340))  # Línea vertical izquierda
-pygame.draw.rect(pantalla, blanco, (320, 42, 10, 340))  # Línea vertical derecha
-pygame.draw.rect(pantalla, blanco, (42, 140, 400, 10))  # Línea horizontal superior
-pygame.draw.rect(pantalla, blanco, (42, 280, 400, 10))  # Línea horizontal inferior
 
 def box(x, y):
-    return pygame.Rect(x, y, 110, 110)
-
-cuadradoSuperiorIzquierdo = box(45,30)
-cuadradoSuperiorDerecho = box(190,30)
-cuadradoSuperiorCentral = box(330,30)
-
-cuadradoCentralIzquierdo = box(45,160)
-cuadradoCentralCentral = box(190,160)
-cuadradoCentralDerecho = box(330,160)
-
-cuadradoInferiorIzquierdo = box(45,290)
-cuadradoInferiorCentral = box(190,290)
-cuadradoInferiorDerecho = box(330,290)
-
-
-pygame.draw.rect(pantalla, negro, cuadradoSuperiorIzquierdo)  # Rectángulo con borde
-pygame.draw.rect(pantalla, negro, cuadradoSuperiorCentral)
-pygame.draw.rect(pantalla, negro, cuadradoSuperiorDerecho)
-
-pygame.draw.rect(pantalla, negro, cuadradoCentralIzquierdo)
-pygame.draw.rect(pantalla, negro, cuadradoCentralCentral)
-pygame.draw.rect(pantalla, negro, cuadradoCentralDerecho)
-
-pygame.draw.rect(pantalla, negro, cuadradoInferiorIzquierdo)
-pygame.draw.rect(pantalla, negro, cuadradoInferiorCentral)
-pygame.draw.rect(pantalla, negro, cuadradoInferiorDerecho)
+    box = pygame.Rect(x, y, 110, 110)
+    pygame.draw.rect(pantalla, negro, box)   
+    return box
 
 def tablero():
     tablero = [(0,0,0), #0.0 0.1 0.2
-               (0,0,0), #1.0 1.1 1.2
-               (0,0,0)] #2.0 2.1 2.2
-    return tablero
+                (0,0,0), #1.0 1.1 1.2
+                (0,0,0)] #2.0 2.1 2.2
+
+    lineas = [(160, 42, 10, 340),
+                (320, 42, 10, 340),
+                (42, 140, 400, 10),
+                (42, 280, 400, 10)]
+        
+    x = [45 , 190, 330]
+    y = [30 , 160, 290]
+        
+    boxes = []       
+    for i in range(len(lineas)):
+        pygame.draw.rect(pantalla, blanco, lineas[i])
+
+    for i in range(len(x)):
+        for j in range(len(y)):
+            caja = box(x[i], y[j])
+            boxes.append(caja)
+
+    return tablero, boxes
 
 
-'''
-def player1():
 
-def player2():
-'''   
+def juego(img, cajas):
+    corriendo = True
+    while corriendo:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()  # Asegura que el programa salga completamente
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()  # Obtener la posición del mouse
+                for i, caja in enumerate(cajas):
+                    if caja.collidepoint(pos):  # Verificar si se hizo clic en una caja
+                        # Dibujar la imagen en la caja correspondiente
+                        pantalla.blit(img, (caja.x + 5, caja.y + 5))
+                        return i  # Retorna el índice de la caja seleccionada
+
+        pygame.display.update()
+
+
+def player1(cajas):
+    equis = pygame.image.load('cruz.png')   
+    juego(equis , cajas)
+
+def player2(cajas):
+    circulo = pygame.image.load('circulo.png')
+    juego(circulo, cajas)
+
 def turno():
+    tabla, cajas = tablero()
     turno = 0
-    while true:
-        if turno == 0:
-            player1()
-        else:
-            player2()
-        if check_win() == True:
+    while True:
+        if turno > 9:
+            print("Empate")
             break
-
-
-
-def check_win():
-    if tablero[0][0] == tablero[0][1] == tablero[0][2]:
-        return True
-    elif tablero[1][0] == tablero[1][1] == tablero[1][2]:
-        return True
-    elif tablero[2][0] == tablero[2][1] == tablero[2][2]:
-        return True
-    elif tablero[0][0] == tablero[1][0] == tablero[2][0]:
-        return True
-    elif tablero[0][1] == tablero[1][1] == tablero[2][1]:
-        return True
-    elif tablero[0][2] == tablero[1][2] == tablero[2][2]:
-        return True
-    elif tablero[0][0] == tablero[1][1] == tablero[2][2]:
-        return True
-    elif tablero[0][2] == tablero[1][1] == tablero[2][0]:
-        return True
-
-pygame.display.update()
+        elif turno % 2 == 0:
+            player1(cajas)
+            if check_win(tabla,turno) == True:
+                break
+            turno += 1
+        elif turno % 2 == 1: 
+            player2(cajas)
+            turno += 1
+            if check_win(tabla,turno) == True:
+                break
+        
     
-game_over = False
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
+    return True
 
 
-pygame.quit()
+def check_win(tablero,turno):
+    victoria = False
+    if tablero[0][0] == tablero[0][1] == tablero[0][2] == 1:
+        victoria = True
+    elif tablero[1][0] == tablero[1][1] == tablero[1][2] == 1:
+        victoria = True
+    elif tablero[2][0] == tablero[2][1] == tablero[2][2] == 1:
+        victoria = True
+    elif tablero[0][0] == tablero[1][0] == tablero[2][0] == 1:
+        victoria = True
+    elif tablero[0][1] == tablero[1][1] == tablero[2][1] == 1:
+        victoria = True
+    elif tablero[0][2] == tablero[1][2] == tablero[2][2] == 1:
+        victoria = True
+    elif tablero[0][0] == tablero[1][1] == tablero[2][2] == 1:
+        victoria = True
+    elif tablero[0][2] == tablero[1][1] == tablero[2][0] == 1:
+        victoria = True
+
+    if victoria == True:
+        if turno % 2 == 0:
+            print('Gana el jugador 1')
+        elif turno % 2 == 1:
+            print('Gana el jugador 2')
+    return victoria
+
+
+def game():
+    
+    
+    turno() 
+    pygame.display.update()
+    
+    game_over = False
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+
+
+    pygame.quit()
+
+game()
